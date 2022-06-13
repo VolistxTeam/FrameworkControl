abb81c2024f1af1ce2e77533628259b69bdf7819
Cryental <contact@cryental.com>
2022-06-13 07:19:46 -0400

    Change to Laravel/Lumen Package

---
+++ src/FrameworkControlServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Volistx\FrameworkControl;
+
+use Illuminate\Support\ServiceProvider;
+
+class FrameworkControlServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+use Volistx\FrameworkControl\FrameworkControlServiceProvider;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        // additional setup
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            FrameworkControlServiceProvider::class,
+        ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // perform environment setup
+    }
+}
