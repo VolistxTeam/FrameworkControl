a32433794a5bc4d88a00c29741b36a8f36cd5db9
I'm Not Cry <44664655+Cryental@users.noreply.github.com>
2022-06-23 02:55:40 +0600

    Update RequestHelper.php

--- src/Helpers/RequestHelper.php
+++ src/Helpers/RequestHelper.php
@@ -44,8 +44,6 @@ class RequestHelper
 
             return new ProcessedResponse($response);
         } catch (ClientException|GuzzleException $ex) {
-            ray($ex);
-
             return new ProcessedResponse($ex);
         }
     }
