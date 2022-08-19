b1134b78af1b6a69a6c39dbe4b4a5efa98d0ed4c
Scorias <mostafa.samy203@gmail.com>
2022-08-19 03:26:40 +0200

    more bug fixs.

--- src/Instances/ProcessedResponse.php
+++ src/Instances/ProcessedResponse.php
@@ -15,23 +15,23 @@ class ProcessedResponse
 
     public function __construct($response)
     {
-        $this->status_code = $response->getCode();
 
         if ($response instanceof (ResponseInterface::class)) {
             $this->headers = $response->getHeaders();
             $this->body = json_decode($response->getBody()->getContents(), true);
-
+            $this->status_code = $response->getCode();
             return;
         }
 
         if ($response instanceof (ClientException::class) || $response instanceof (BadResponseException::class)) {
             $this->headers = $response->getResponse()->getHeaders();
             $this->body = json_decode($response->getResponse()->getBody()->getContents(), true);
-
+            $this->status_code = $response->getCode();
             return;
         }
 
         if ($response instanceof (GuzzleException::class)) {
+            $this->status_code = 500;
             $this->headers = null;
             $this->body = null;
         }
